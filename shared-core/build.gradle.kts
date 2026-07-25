71344881c6037bd18445085148105d5fce8f2ce2
Shivayogi Hiremath <shivayogih72@gmail.com>
2026-07-25 18:15:21 +0530

    [TLY-012A] Add provider-neutral shared core module

---
+++ shared-core/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+
+    jvmToolchain(17)
+}
+
+android {
+    namespace = "com.toolly.shared.core"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 23
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencyLocking {
+    lockAllConfigurations()
+}
