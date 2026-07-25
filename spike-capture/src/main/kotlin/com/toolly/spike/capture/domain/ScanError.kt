8fd094bc0b381b1245bc1f54b77379cf93e6f885
Shivayogi Hiremath <shivayogih72@gmail.com>
2026-07-25 18:28:51 +0530

    [TLY-012A] Remove migrated Android capture contract

--- spike-capture/src/main/kotlin/com/toolly/spike/capture/domain/ScanError.kt
+++
@@ -1,7 +0,0 @@
-package com.toolly.spike.capture.domain
-
-/** Temporary source-compatibility alias for allowlisted shared capture errors. */
-typealias ScanError = com.toolly.shared.capture.ScanError
-
-/** Temporary source-compatibility alias for allowlisted partial-capture reasons. */
-typealias PartialCaptureReason = com.toolly.shared.capture.PartialCaptureReason
